05d2ab5a74bf2079978331c4005bc2c20ca1ccaf
herdy3 <herdy@plabs.id>
2024-12-16 23:13:03 +0700

    Add camera functionality and navigation link in dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/ui/app-sidebar";
 import { Toaster } from "sonner";
+import { Camera } from "lucide-react";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -11,9 +13,19 @@ export default function RootLayout({
     <SidebarProvider>
       <AppSidebar />
       <div className="w-full">
-        <div className="w-full p-6 border-b flex items-center gap-2">
-          <SidebarTrigger />
-          <h2 className="plabs-headline-semibold-24">SakuraCrop Defender</h2>
+        <div className="w-full p-6 border-b flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger />
+            <h2 className="plabs-headline-semibold-24">SakuraCrop Defender</h2>
+          </div>
+          <div className="flex items-center gap-2">
+            <Link
+              href="/scan"
+              className="plabs-caption-bold-14 flex items-center gap-2 btn btn-outline-greyscale"
+            >
+              <Camera /> Take a Picture
+            </Link>
+          </div>
         </div>
         <div className="p-6">{children}</div>
       </div>
--- src/app/(detection)/scan/camera.tsx
+++ src/app/(detection)/scan/camera.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
 import { Camera as CameraPro } from "react-camera-pro";
-import { Camera as CameraIcon, Check, X } from "lucide-react";
+import { Camera as CameraIcon, Check, X, ArrowLeft } from "lucide-react";
 import { base64ToBlob } from "@/lib/file";
 import { useForm } from "react-hook-form";
 import { InsertScans, Scans } from "@/server/_schema/scans";
@@ -9,11 +9,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import Form from "@/components/ui/form";
 import { toast } from "@/components/ui/alert/toast";
 import { serviceCreateScans } from "@/server/_services/scans";
+import Link from "next/link";
 
 export const Camera = ({ plants }: { plants: any }) => {
   const camera = useRef<any>(null);
   const [image, setImage] = useState<string | null>(null);
   const [detecting, setDetecting] = useState<any>({});
+  const [loading, setLoading] = useState(false);
 
   const form = useForm<Scans>({
     defaultValues: {
@@ -55,6 +57,8 @@ export const Camera = ({ plants }: { plants: any }) => {
   }, [image]);
 
   async function onSave(data: any) {
+    setLoading(true);
+
     const detection = detecting?.detections.map((detection: any) => {
       return {
         label: detection.label,
@@ -85,88 +89,127 @@ export const Camera = ({ plants }: { plants: any }) => {
         });
       }
     }
+
+    setLoading(false);
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-end">
-      <CameraPro
-        ref={camera}
-        facingMode="environment"
-        errorMessages={{
-          noCameraAccessible:
-            "No camera device accessible. Please connect your camera or try a different browser.",
-          permissionDenied:
-            "Permission denied. Please refresh and give camera permission.",
-          switchCamera:
-            "It is not possible to switch camera to different one because there is only one video device accessible.",
-          canvas: "Canvas is not supported.",
-        }}
-      />
-      <button
-        onClick={() => {
-          if (camera.current) {
-            setImage(camera.current.takePhoto());
-          }
-        }}
-        className="fixed bottom-4 z-50 bg-white text-black p-4 rounded-full"
-      >
-        <CameraIcon size={30} />
-      </button>
-      {image && (
-        <div className="fixed p-4 top-0 right-0 z-50">
-          <div className="relative w-full h-full shadow-lg border border-white rounded-lg overflow-hidden">
-            <img
-              src={
-                (detecting?.detections?.length > 0 &&
-                  process.env.NEXT_PUBLIC_API_URL +
-                    "/" +
-                    detecting.image_path) ||
-                image
-              }
-              alt="Taken photo"
-              className="object-cover w-full h-full"
-            />
-            <Form form={form} onSave={onSave}>
-              {detecting?.detections?.length > 0 && (
-                <div className="absolute bottom-3 left-3 flex items-center gap-2">
-                  <Form.Select
-                    className="w-[150px] md:w-[200px]"
-                    placeholder="Select plant"
-                    name="plant_id"
-                    options={plants.data.map((plant: any) => {
-                      return {
-                        value: plant.id,
-                        label: plant.name,
-                      };
-                    })}
-                  />
-                </div>
-              )}
-              <div className="absolute bottom-3 right-3 flex items-center gap-2">
-                <Form.Button
-                  onClick={() => {
-                    setImage(null);
-                    setDetecting({});
-                    form.reset();
-                  }}
-                  className=" bg-red-500 text-white p-2 border rounded-full"
-                  type="button"
-                >
-                  <X size={20} />
-                </Form.Button>
-                {detecting?.detections?.length > 0 && (
-                  <Form.Button
-                    className=" bg-green-500 text-white p-2 border rounded-full"
-                    type="submit"
-                  >
-                    <Check size={20} />
-                  </Form.Button>
-                )}
-              </div>
-            </Form>
+    <>
+      {loading && (
+        <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-[9999] flex items-center justify-center">
+          <div className="bg-white p-4 rounded-lg">
+            <svg
+              className="animate-spin h-10 w-10 text-blue-base"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A8.001 8.001 0 0120.709 5.709L18.296 8.12A6 6 0 005.88 18.296l2.412-2.412z"
+              ></path>
+            </svg>
           </div>
         </div>
       )}
-    </div>
+      <div className="w-screen h-screen flex flex-col items-center justify-end">
+        <CameraPro
+          ref={camera}
+          facingMode="environment"
+          errorMessages={{
+            noCameraAccessible:
+              "No camera device accessible. Please connect your camera or try a different browser.",
+            permissionDenied:
+              "Permission denied. Please refresh and give camera permission.",
+            switchCamera:
+              "It is not possible to switch camera to different one because there is only one video device accessible.",
+            canvas: "Canvas is not supported.",
+          }}
+        />
+        <Link
+          href="/farm-conditions"
+          className="fixed top-4 left-4 z-50 flex items-center gap-2 plabs-caption-bold-12 text-blue-base underline bg-white px-4 py-2 rounded-md"
+        >
+          <ArrowLeft size={16} /> Back to List Farm Conditions
+        </Link>
+        <button
+          onClick={() => {
+            if (camera.current) {
+              setImage(camera.current.takePhoto());
+            }
+          }}
+          className="fixed bottom-4 z-50 bg-white text-black p-4 rounded-full"
+        >
+          <CameraIcon size={30} />
+        </button>
+        {image && (
+          <>
+            <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-[100]" />
+            <div className="fixed p-4 top-0 right-0 z-[101]">
+              <div className="relative w-full max-w-[350px] max-h-[500px] shadow-lg border border-white rounded-lg overflow-hidden">
+                <img
+                  src={
+                    (detecting?.detections?.length > 0 &&
+                      process.env.NEXT_PUBLIC_API_URL +
+                        "/" +
+                        detecting.image_path) ||
+                    image
+                  }
+                  alt="Taken photo"
+                  className="object-cover w-full h-full"
+                />
+                <Form form={form} onSave={onSave}>
+                  {detecting?.detections?.length > 0 && (
+                    <div className="absolute bottom-3 left-3 flex items-center gap-2">
+                      <Form.Select
+                        // className="min-w-[150px] md:min-w-[200px]"
+                        placeholder="Select plant"
+                        name="plant_id"
+                        options={plants.data.map((plant: any) => {
+                          return {
+                            value: plant.id,
+                            label: plant.name,
+                          };
+                        })}
+                      />
+                    </div>
+                  )}
+                  <div className="absolute bottom-3 right-3 flex items-center gap-2">
+                    <Form.Button
+                      onClick={() => {
+                        setImage(null);
+                        setDetecting({});
+                        form.reset();
+                      }}
+                      className=" bg-red-500 text-white p-2 border rounded-full"
+                      type="button"
+                    >
+                      <X size={20} />
+                    </Form.Button>
+                    {detecting?.detections?.length > 0 && (
+                      <Form.Button
+                        className=" bg-green-500 text-white p-2 border rounded-full"
+                        type="submit"
+                      >
+                        <Check size={20} />
+                      </Form.Button>
+                    )}
+                  </div>
+                </Form>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
