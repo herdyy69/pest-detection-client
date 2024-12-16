c6c119ff43277657d1869e220b62cc5da492c456
herdy3 <herdy@plabs.id>
2024-12-16 13:47:40 +0700

    Add statistics page to retrieve and display plant and scan data

---
+++ src/app/(dashboard)/statistics/log.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+export const Log = (data: any) => {
+  console.log(data);
+
+  return <div>Log</div>;
+};
--- src/app/(dashboard)/statistics/page.tsx
+++ src/app/(dashboard)/statistics/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <div>xxx</div>;
+import { servicePlants } from "@/server/_services/plants";
+import { serviceScans } from "@/server/_services/scans";
+import { Log } from "./log";
+
+export default async function Page() {
+  const data = await serviceScans({ page: 1, limit: 9999, search: "" });
+  const plants = await servicePlants({ page: 1, limit: 9999, search: "" });
+
+  return (
+    <div>
+      <Log data={plants} />
+    </div>
+  );
 }
